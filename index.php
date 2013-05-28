14bc4aa1c2e39300889b5d5c77f5f0f99a0ad34e
narcislinux <3006nol@gmail.com>
2013-05-28 15:49:13 +0430

    change background and a coler

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <body>
 
 <!-- #header -->
-<div class="container-fluid hidden-phone">
+<div class="container-fluid">
 	<div id="header"  >
 	<div class="row-fluid">
  		<div class="span12">
@@ -55,52 +55,53 @@
 <!-- #post -->
 <div class="container-fluid" >
 <div class="row-fluid">
-		<div class="span8">
-			<div id="all-post">
-	 			<?php if ( have_posts() ) : ?>
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-						
-						<div class="post_kol"  id="post-<?php the_id(); ?>" >	
-							<h3><a href="<?php the_permalink(); ?>"  title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-				
-							<div class="post_neveshteh">
-							<?php the_content(); ?>
+			<div class="span8">
+				<div id="all-post">
+		 			<?php if ( have_posts() ) : ?>
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+							
+							<div class="post_kol"  id="post-<?php the_id(); ?>" >	
+								<h3><a href="<?php the_permalink(); ?>"  title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+					
+								<div class="post_neveshteh">
+								<?php the_content(); ?>
+								</div>
 							</div>
+
+
+							<small>This entry was posted on 
+								POST TITLE: <?php the_title(); ?><br />
+								AUTHOR: <?php the_author(); ?><br />
+								POSTED: <?php the_time('jS F Y') ?><br />
+								FILED AS: <?php the_category(', ') ?><br />
+								COMMENT FEED: <?php comments_rss_link('RSS 2.0'); ?><br />
+								PREVIOUS: <?php previous_post('%', '', 'yes', 'yes'); ?><br />
+								NEXT: <?php next_post('%', '', 'yes', 'yes'); ?>
+								feed.
+							</small>
+
+						<?php endwhile ; ?>
+
+						<div class="poste_badi_ghabli">
+							<div class="alignleft">
+							<?php previous_post('&laquo; &laquo; %',
+					 		'Toward The Past: ', 'yes'); ?>
+						    </div>
+							<div class="alignright">
+								<?php next_post('% &raquo; &raquo; ',
+								 'Toward The Future: ', 'yes'); ?>
+							</div>
+						</div>	
+				
+					<?php else : ?>
+						<div class="post-nist">
+						<h2><?php _e('Not Found'); ?></h2>
 						</div>
-
-
-						<small>This entry was posted on 
-							POST TITLE: <?php the_title(); ?><br />
-							AUTHOR: <?php the_author(); ?><br />
-							POSTED: <?php the_time('jS F Y') ?><br />
-							FILED AS: <?php the_category(', ') ?><br />
-							COMMENT FEED: <?php comments_rss_link('RSS 2.0'); ?><br />
-							PREVIOUS: <?php previous_post('%', '', 'yes', 'yes'); ?><br />
-							NEXT: <?php next_post('%', '', 'yes', 'yes'); ?>
-							feed.
-						</small>
-
-					<?php endwhile ; ?>
-
-					<div class="poste_badi_ghabli">
-						<div class="alignleft">
-						<?php previous_post('&laquo; &laquo; %',
-				 		'Toward The Past: ', 'yes'); ?>
-					    </div>
-						<div class="alignright">
-							<?php next_post('% &raquo; &raquo; ',
-							 'Toward The Future: ', 'yes'); ?>
-						</div>
-					</div>	
-			
-				<?php else : ?>
-					<div class="post-nist">
-					<h2><?php _e('Not Found'); ?></h2>
-					</div>
-				<?php endif; ?>
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
+		
 			
 <!-- end #post -->
 
