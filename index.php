80c2f6bedd3dce405e698e906f66294ff4fbaca4
narcislinux <3006nol@gmail.com>
2013-05-29 16:50:48 +0430

    change footer

--- index.php
+++ index.php
@@ -174,13 +174,19 @@
 
 		<div class="span12">
 			
-        <p class="pull-right"><a href="#">Back to top</a></p>
 
-	    <p>
-	    	<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" 
-	    	alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
-	  	</p>
 
+		    <p>
+		    	<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" 
+		    	alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+		  	</p>
+
+
+        <div class="footer-iso" >
+
+       	 <p class="pull-right"><a href="#">Back to top</a></p>
+
+	    </div>
 	  	
 	  	
 	  	</div>
